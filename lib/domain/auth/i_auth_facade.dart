531facbc1abc04708b6d437c60c64e47ac83c713
tomonakar <tomohisa.nak@gmail.com>
2022-03-28 16:29:26 +0900

    feat: 認証のインターフェースを作成

--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -8,11 +8,13 @@ import 'auth_failure.dart';
 
 abstract class IAuthFacade {
   Future<Option<User>> getSignedInUser();
-  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword(
-      {@required EmailAddress emailAddress, @required Password password});
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
   Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
-    @required EmailAddress emailAddress,
-    @required Password password,
+    required EmailAddress emailAddress,
+    required Password password,
   });
   Future<Either<AuthFailure, Unit>> signInWithGoogle();
   Future<void> signOut();
