f1a916d321b25229a1ada105b2b1db4eb00b4e56
tomonakar <tomohisa.nak@gmail.com>
2022-03-31 18:20:49 +0900

    feat: usecaseのインターフェース作成

---
+++ lib/domain/todo_list/usecase/i_todo_list_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_samples/domain/todo_list/entity/todo_item.dart';
+
+import '../vo/todo_id.dart';
+
+abstract class ITodoListUsecase {
+  Stream<List<TodoItem>> getTodoList();
+  Future<TodoItem> findByTodoId({required TodoId todoId});
+  Future<void> updateIsDone({required TodoId todoId});
+  Future<void> updateTodoItem({required TodoItem item});
+  Future<void> deleteTodoItem({required TodoId todoId});
+  Future<void> createTodoItem();
+}
