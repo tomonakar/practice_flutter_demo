2040863a7b820a4d8165891e5e58f4a0c59d21a7
tomonakar <tomohisa.nak@gmail.com>
2022-03-31 18:30:21 +0900

    feat: usecaseのproviderを追加

--- lib/providers/todo_item_provider.dart
+++ lib/providers/todo_item_provider.dart
@@ -1,6 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../infra/repository/todo_list_repository.dart';
+import '../usecase/todo_list/todo_list_usecase.dart';
 
 final todoListRepositoryProvider =
     Provider.autoDispose((ref) => TodoListRepository());
+
+final todoListUsecaseProvider = Provider((ref) => TodoListUsecase(
+      todoListRepository: ref.read(todoListRepositoryProvider),
+    ));
